b47e3976bca440ce829b00acdae36ac7779268f0
Unity12 sungjae <sj070600@gmail.com>
2025-12-11 20:15:14 +0900

    feat: 전투 중 애니메이션 중간 커밋

--- Assets/01.Scripts/Player/PlayerAnimation.cs
+++ Assets/01.Scripts/Player/PlayerAnimation.cs
@@ -5,8 +5,14 @@ using UnityEngine.TextCore.Text;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField] private GameObject weaponHand; //손에 칼
+    [SerializeField] private GameObject weaponIn;   //집어넣은 칼
+
     public Rigidbody rb;
     public PhysicsCharacter character;
+    [SerializeField] private PlayerCombat combat;   // 새로 추가
+    [SerializeField] private GameObject weaponHitbox; // 무기 히트박스(콜라이더 오브젝트)
+
     public float runhold = 0.15f;
 
     private Animator anim;
@@ -16,11 +22,17 @@ public class PlayerAnimation : MonoBehaviour
     private static readonly int GroundedHash = Animator.StringToHash("Grounded");
     private static readonly int TestHash = Animator.StringToHash("test");
 
+    private static readonly int WeaponEquippedHash = Animator.StringToHash("WeaponEquipped");
+    private static readonly int ComboIndexHash = Animator.StringToHash("ComboIndex");
+    private static readonly int AttackHash = Animator.StringToHash("Attack");
+    private static readonly int IsAttackingHash = Animator.StringToHash("IsAttacking");
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
         if (rb == null) rb = GetComponent<Rigidbody>();
         if (character == null) character = GetComponent<PhysicsCharacter>();
+        if (combat == null) combat = GetComponent<PlayerCombat>();
     }
 
     private void Update()
@@ -50,4 +62,57 @@ public class PlayerAnimation : MonoBehaviour
     {
         anim.SetTrigger(TestHash);
     }
-}
+
+    public void Attack(int comboIndex)
+    {
+        anim.SetInteger(ComboIndexHash, comboIndex);
+        anim.SetTrigger(AttackHash);
+    }
+
+    public void SetIsAttacking(bool value)
+    {
+        anim.SetBool(IsAttackingHash, value);
+    }
+    public void SetWeaponEquipped(bool equipped)
+    {
+        anim.SetBool(WeaponEquippedHash, equipped);
+
+        if (weaponHand != null)
+        {
+            weaponHand.SetActive(equipped);
+        }
+
+        if (weaponIn != null)
+        {
+            weaponIn.SetActive(equipped);
+        }
+    }
+
+    public void EvAttackMoveForward()
+    {
+        if (combat != null)
+            combat.OnAttackMoveForwardFromAnim();
+    }
+
+    public void EvAttackAnimationEnd()
+    {
+        if (combat != null)
+            combat.OnAttackAnimationEndFromAnim();
+    }
+
+    public void EvAttackHitboxDisable()
+    {
+        if (weaponHitbox != null)
+            weaponHitbox.SetActive(false);
+    }
+
+    public void EvAttackHitboxEnable()
+    {
+        if (weaponHitbox != null)
+            weaponHitbox.SetActive(true);
+    }
+
+    }
+
+
+
