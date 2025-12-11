757aa1c08cb4341dfc9307cc11661e0d718d4578
김동관 <kdk7992@gmail.com>
2025-12-11 20:48:09 +0900

    feat: Status 연결작업

--- Assets/01.Scripts/Player/PlayerStat.cs
+++ Assets/01.Scripts/Player/PlayerStat.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PlayerUIStatus ui;
+    private CharacterBase character;   
+
+    public int level = 1;
+    
+    public float maxStamina = 100f;
+    public float currentStamina = 100f;
+
+    public float maxExp = 100f;
+    public float currentExp = 0f;
+
+    private void Awake()
     {
-        
+        character = GetComponent<CharacterBase>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        UpdateAllUI();    
+    }
+
+    private void Update()
+    {
+        ui.UpdateHp(character.currentHp, character.maxHp);
+        ui.UpdateStamina(currentStamina, maxStamina);
+        ui.UpdateExp(currentExp, maxExp);
+        ui.UpdateLevel(level);
+    }
+
+    private void UpdateHpUI()
+    {
+        if (character == null) return;
+        ui.UpdateHp(character.currentHp, character.maxHp);
+    }
+
+    private void UpdateAllUI()
+    {
+        ui.UpdateHp(character.currentHp, character.maxHp);
+        ui.UpdateLevel(level);       
     }
 }
