01c816108c61dd9cc58b686fa1ecd5f143fa0124
김동관 <kdk7992@gmail.com>
2025-12-12 19:19:46 +0900

    feat: 맵 1차 수정

--- Assets/01.Scripts/UI/MiniMapZoomController.cs
+++ Assets/01.Scripts/UI/MiniMapZoomController.cs
@@ -11,7 +11,7 @@ public class MiniMapZoomController : MonoBehaviour
     
     [Header("카메라 버튼 셋팅")]
     [SerializeField] private Camera miniMapCamera;
-    
+ 
     //시작시 미니맵 기본값 저장
     private Vector2 defaultSizeDelta;
     private Vector2 defaultAnchoredPos;
@@ -27,5 +27,6 @@ public class MiniMapZoomController : MonoBehaviour
         defaultAnchoredPos = miniMapImage.anchoredPosition;
         defaultArrowPos = playerArrow.anchoredPosition;
     }
+    
     
 }
