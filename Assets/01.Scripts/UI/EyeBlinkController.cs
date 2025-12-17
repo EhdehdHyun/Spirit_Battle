93c1ebaf893e411556b14daee5fad8cf09452e62
김동관 <kdk7992@gmail.com>
2025-12-17 21:00:55 +0900

    feat: IntroScene뼈대 완성

--- Assets/01.Scripts/UI/EyeBlinkController.cs
+++ Assets/01.Scripts/UI/EyeBlinkController.cs
@@ -1,10 +1,20 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using TMPro;
 
 public class EyeBlinkController : MonoBehaviour
 {
+    [Header("Eye Blink")]
     public Image eyeImage;
+
+    [Header("Dialogue")]
+    public TextMeshProUGUI dialogueText;
+
+    [Header("Camera")]
+    public Transform cameraTransform;
+    public Quaternion leftRotation;
+    public Quaternion rightRotation;
 
     void Start()
     {
@@ -13,30 +23,45 @@ public class EyeBlinkController : MonoBehaviour
 
     IEnumerator BlinkRoutine()
     {
-        // 완전 감김
+        // 첫 눈 깜빡임 
         SetAlpha(1f);
-        yield return new WaitForSeconds(1.5f);
+        dialogueText.text = "Fuck... What happened?";
+        yield return new WaitForSeconds(3f);
 
-        // 깜빡 2~3회
         yield return Blink();
-        yield return Blink();
-
-        // 완전히 눈 뜸
         yield return Fade(1f, 0f, 2f);
+
+        //왼쪽 회전 + 눈 감기 
+        yield return RotateCamera(cameraTransform.rotation, leftRotation, 1f);
+
+        dialogueText.text = "I remember to...…";
+        yield return Fade(0f, 1f, 0.5f);
+        yield return new WaitForSeconds(1f);
+
+        //오른쪽 회전 + 눈 뜸 
+        yield return RotateCamera(leftRotation, rightRotation, 1.5f);
+
+        dialogueText.text = "what's that?";
+        yield return Fade(1f, 0f, 1.5f);
+
+        yield return new WaitForSeconds(2f);
+        dialogueText.text = "";
+
         gameObject.SetActive(false);
     }
 
+    //눈 깜빡임 
     IEnumerator Blink()
     {
-        yield return Fade(1f, 0.3f, 1f);
-        yield return new WaitForSeconds(0.3f);
-        yield return Fade(0.3f, 1f, 2f);
-        yield return new WaitForSeconds(1f);
+        yield return Fade(1f, 0.3f, 0.8f);
+        yield return new WaitForSeconds(0.2f);
+        yield return Fade(0.3f, 1f, 1f);
+        yield return new WaitForSeconds(0.5f);
     }
 
     IEnumerator Fade(float from, float to, float time)
     {
-        float t = 0;
+        float t = 0f;
         while (t < time)
         {
             t += Time.deltaTime;
@@ -44,12 +69,22 @@ public class EyeBlinkController : MonoBehaviour
             yield return null;
         }
     }
-    
-    
 
     void SetAlpha(float a)
     {
-        var c = eyeImage.color;
+        Color c = eyeImage.color;
         eyeImage.color = new Color(c.r, c.g, c.b, a);
+    }
+
+    // 카메라 회전 
+    IEnumerator RotateCamera(Quaternion from, Quaternion to, float time)
+    {
+        float t = 0f;
+        while (t < time)
+        {
+            t += Time.deltaTime;
+            cameraTransform.rotation = Quaternion.Slerp(from, to, t / time);
+            yield return null;
+        }
     }
 }
