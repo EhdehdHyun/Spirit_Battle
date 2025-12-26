997ca0e716b8b028f6fe5915a7653c95ea2c5bbf
김동관 <kdk7992@gmail.com>
2025-12-26 15:36:32 +0900

    feat: 화살표, 상자 Interact수정

--- Assets/01.Scripts/UI/BoxTutorialTrigger.cs
+++ Assets/01.Scripts/UI/BoxTutorialTrigger.cs
@@ -5,20 +5,8 @@ public class BoxTutorialTrigger : MonoBehaviour
 {
     [SerializeField] private DialogueCameraController cameraController;
     [SerializeField] private Transform boxCameraPoint;
-    [SerializeField] private BoxInteract boxInteract;
-   // [SerializeField] private MonoBehaviour playerMovement;
-
-    [Header("Guide Text")]
-    [SerializeField] private GameObject guideText;
-    
 
     private bool triggered = false;
-
-    private void Awake()
-    {
-        if (guideText != null)
-            guideText.SetActive(false);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,24 +14,18 @@ public class BoxTutorialTrigger : MonoBehaviour
         if (!other.CompareTag("Player")) return;
 
         triggered = true;
-        //worldArrow.gameObject.SetActive(true);
         StartCoroutine(TutorialSequence());
     }
 
     private IEnumerator TutorialSequence()
     {
-        if (boxInteract == null)
+        if (cameraController == null || boxCameraPoint == null)
         {
-            Debug.LogError("[BoxTutorialTrigger] boxInteract is NULL");
+            Debug.LogError("[BoxTutorialTrigger] CameraController or CameraPoint is NULL");
             yield break;
         }
 
-        boxInteract.enabled = false;
-
         cameraController.FocusOnce(boxCameraPoint, 1.3f);
         yield return new WaitForSeconds(2f);
-
-        boxInteract.enabled = true;
     }
-
 }
