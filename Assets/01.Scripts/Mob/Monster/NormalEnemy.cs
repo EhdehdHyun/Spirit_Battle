d2edc42b04a2fbe30038d0c0a6cb2e63603f7918
Bari_box. <dhkang0126@naver.com>
2025-12-29 14:32:56 +0900

    feat : 보스 몬스터 3페이즈 애니메이션 수정

--- Assets/01.Scripts/Mob/Monster/NormalEnemy.cs
+++ Assets/01.Scripts/Mob/Monster/NormalEnemy.cs
@@ -5,6 +5,9 @@ public class NormalEnemy : EnemyBase
     [Header("애니메이션")]
     [SerializeField] private MonsterAnimation monsterAnim;
 
+    [Header("피격 피드백")]
+    [SerializeField] private DamageFeedback damageFeedback;
+
     [Header("피격 시 Hit 트리거 사용")]
     [SerializeField] private bool playHitOnDamaged = true;
 
@@ -14,12 +17,18 @@ public class NormalEnemy : EnemyBase
 
         if (monsterAnim == null)
             monsterAnim = GetComponent<MonsterAnimation>() ?? GetComponentInChildren<MonsterAnimation>(true);
+
+        if (damageFeedback == null)
+            damageFeedback = GetComponentInChildren<DamageFeedback>(true);
     }
 
     protected override void OnDamaged(DamageInfo info)
     {
         base.OnDamaged(info);
         if (IsDead) return;
+
+        damageFeedback?.Play();
+
 
         if (playHitOnDamaged)
             monsterAnim?.TryPlayHit();
