2b9fc6baa711bd7888e150d8c05bcd14993b5646
김동관 <kdk7992@gmail.com>
2026-01-19 14:11:00 +0900

    feat: 월드맵에 화살표 아이콘 추가

--- Assets/01.Scripts/UI/Map/WorldMapPlayerIcon.cs
+++ Assets/01.Scripts/UI/Map/WorldMapPlayerIcon.cs
@@ -14,6 +14,7 @@ public class WorldMapPlayerIcon : MonoBehaviour
     private void Update()
     {
         UpdatePlayerIcon();
+        UpdatePlayerRotation();
     }
 
     private void UpdatePlayerIcon()
@@ -27,5 +28,17 @@ public class WorldMapPlayerIcon : MonoBehaviour
         float mapY = (ny - 0.5f) * mapImage.rect.height;
 
         playerIcon.anchoredPosition = new Vector2(mapX, mapY);
+    }
+    private void UpdatePlayerRotation()
+    {
+        Vector3 forward = player.forward;
+
+        // Z가 위, X가 오른쪽 기준
+        float angle = Mathf.Atan2(forward.z, forward.x) * Mathf.Rad2Deg;
+
+        // 아이콘 기본 방향이 왼쪽
+        float uiRotation = angle + 180f;
+
+        playerIcon.localRotation = Quaternion.Euler(0f, 0f, uiRotation);
     }
 }
